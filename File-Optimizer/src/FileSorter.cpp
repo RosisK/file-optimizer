832fc5ce7bdeac10b3feca97b04b4cdfb20a0b67
RosisK <rosiskharel5@gmail.com>
2026-03-02 21:51:36 +0545

    Implemented sorting

--- File-Optimizer/src/FileSorter.cpp
+++ File-Optimizer/src/FileSorter.cpp
@@ -3,5 +3,19 @@
 
 void FileSorter::sortByName(std::vector<FileInfo>& items)
 {
+    std::sort(items.begin(), items.end(),
+        [](const FileInfo& a, const FileInfo& b)
+        {
+            return a.name < b.name;
+        });
+}
 
+
+void FileSorter::sortBySize(std::vector<FileInfo>& items)
+{
+    std::sort(items.begin(), items.end(),
+        [](const FileInfo& a, const FileInfo& b)
+        {
+            return a.size < b.size;
+        });
 }
