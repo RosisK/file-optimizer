17df43743de222a46fede8333ba62cc0ba5770c0
RosisK <rosiskharel5@gmail.com>
2026-03-03 17:22:39 +0545

    Created a sort key enum and sort options struct

--- File-Optimizer/src/FileSort.h
+++ File-Optimizer/src/FileSort.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include "FileInfo.h"
+
+enum class FileSortKey
+{
+	Name,
+	Size,
+	ModifiedTime
+};
+
+struct FileSortOptions
+{
+	FileSortKey key = FileSortKey::Name;
+	bool ascending = true;
+	bool directoriesFirst = true;
+};
