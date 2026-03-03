59e156665fba30aca57dea5de9bd9bebeb133550
RosisK <rosiskharel5@gmail.com>
2026-03-03 17:24:12 +0545

    Changing implementation of FileSorter function

--- File-Optimizer/src/FileSorter.h
+++ File-Optimizer/src/FileSorter.h
@@ -2,10 +2,10 @@
 
 #include <vector>
 #include "FileInfo.h"
+#include "FileSort.h"
 
 class FileSorter
 {
 public:
-	static void sortByName(std::vector<FileInfo>& items);
-	static void sortBySize(std::vector<FileInfo>& items);
+	static void sort(std::vector<FileInfo>& items, const FileSortOptions& options);
 };
