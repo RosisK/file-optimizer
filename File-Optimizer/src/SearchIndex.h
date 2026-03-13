7e9cc79d315449a23bd8330a7b39b6cf240211eb
RosisK <rosiskharel5@gmail.com>
2026-03-13 10:40:39 +0545

    define the inverted index

--- File-Optimizer/src/SearchIndex.h
+++ File-Optimizer/src/SearchIndex.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include "FileInfo.h"
+
+class SearchIndex
+{
+private:
+	std::unordered_map<std::string, std::vector<FileInfo>> index;
+	
+	std::vector<std::string> tokenize(const std::string& text);
+
+public:
+	void buildIndex(const std::vector<FileInfo>& items);
+	
+	std::vector<FileInfo> search(const std::string& query);
+};
