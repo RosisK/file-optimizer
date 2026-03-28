5c7ab8c619cf646428ad95a49d3a4da78a9cd18b
Thanatos0-ops <pradhanaayam91@gmail.com>
2026-03-28 16:13:01 +0545

    Add ZstdCompressor.h with function declarations

---
+++ File-Optimizer/src/ZstdCompressor.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+
+// Function declarations
+bool compressFile(const std::string& inputPath, const std::string& outputPath);
+bool decompressFile(const std::string& inputPath, const std::string& outputPath);
